fc2e8dd1ec52140e19f52065d5df474ecdcae5c9
Chris Brown <chris.brown@nwyc.com>
2013-10-15 23:49:48 -0500

    fixed release version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='ImageResolver',
-	version='0.1.0',
+	version='0.1.1',
 	author='Chris Brown',
 	author_email='chris.brown@nwyc.com',
 	packages=['imageresolver','imageresolver.test','imageresolver.abpy'],
