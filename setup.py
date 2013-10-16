b6fffb0940608928bc92f77d2ad0544fe701d70c
Chris Brown <chris.brown@nwyc.com>
2013-10-15 22:24:58 -0500

    fixed a bug in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
 	version='0.1.0',
 	author='Chris Brown',
 	author_email='chris.brown@nwyc.com',
-	packages=['imageresolver','imageresolver.test','imageresolver.abpy','beautifulsoup4'],
+	packages=['imageresolver','imageresolver.test','imageresolver.abpy'],
 	scripts=['bin/example.py'],
 	url='https://github.com/chrisbrownnwyc/ImageResolverPython',
 	license='LICENSE.txt',
 	description="Find the most significant image in an article.",
 	long_description=open('README.md').read(),
-	install_requires=[ "requests >= 1.0.0" ],
+	install_requires=[ "requests >= 1.0.0","beautifulsoup4" ],
 )
