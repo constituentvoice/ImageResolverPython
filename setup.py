e6ae4da736d52194e62a40d2da6df9a2bffad2e6
Chris Brown <chris.brown@nwyc.com>
2020-06-26 16:18:47 -0500

    cleaned up setup

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
+from __future__ import absolute_import
 from distutils.core import setup
-from distutils.sysconfig import get_python_lib
-import os
-import sys
 
 # attempt to pull version
-sys.path.append('./imageresolver')
-from version import __version__
+from imageresolver.version import __version__
 
 setup(
 	name='ImageResolver',
 	version=__version__,
 	author='Chris Brown',
 	author_email='chris.brown@nwyc.com',
-	packages=['imageresolver','imageresolver.abpy','imageresolver.plugins'],
+	packages=['imageresolver', 'imageresolver.abpy', 'imageresolver.plugins'],
 	package_data={'imageresolver': ['data/*.txt']},
-	scripts=['bin/resolveimg.py'],
 	url='https://github.com/constituentvoice/ImageResolverPython',
 	license='BSD',
 	description="Find the most significant image in an article.",
 	long_description=open('README.rst').read(),
-	install_requires=[ "requests >= 1.0.0","beautifulsoup4" ],
+	install_requires=["requests >= 1.0.0", "beautifulsoup4"],
 	test_suite='tests'
 )
