98823c525677a62579e40f337f40d1c9014736c8
Chris Brown <chris.brown@nwyc.com>
2023-01-18 12:52:09 -0600

    fixed version info

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from __future__ import absolute_import
 from distutils.core import setup
 
-# attempt to pull version
-from imageresolver.version import __version__
+# read the version directly instead of importing it
+exec(open("imageresolver/version.py").read())
 
 setup(
-	name='ImageResolver',
-	version=__version__,
-	author='Chris Brown',
-	author_email='chris.brown@nwyc.com',
-	packages=['imageresolver', 'imageresolver.abpy', 'imageresolver.plugins'],
-	package_data={'imageresolver': ['data/*.txt']},
-	url='https://github.com/constituentvoice/ImageResolverPython',
-	license='BSD',
-	description="Find the most significant image in an article.",
-	long_description=open('README.rst').read(),
-	install_requires=["requests >= 1.0.0", "beautifulsoup4"],
-	test_suite='tests'
+    name='ImageResolver',
+    version=__version__,
+    author='Chris Brown',
+    author_email='chris.brown@nwyc.com',
+    packages=['imageresolver', 'imageresolver.abpy', 'imageresolver.plugins'],
+    package_data={'imageresolver': ['data/*.txt']},
+    url='https://github.com/constituentvoice/ImageResolverPython',
+    license='BSD',
+    description="Find the most significant image in an article.",
+    long_description=open('README.rst').read(),
+    install_requires=["requests >= 1.0.0", "beautifulsoup4"],
+    test_suite='tests'
 )
